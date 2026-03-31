74467eb06a3b6643db082cd4627ff8332c0c870c
Lucas Hector <1lucashector@gmail.com>
2026-03-31 12:38:29 -0300

    fix: adiciona TURN servers para WebRTC em redes diferentes

--- app/sala/[sala_id]/page.tsx
+++ app/sala/[sala_id]/page.tsx
@@ -10,7 +10,9 @@ const sb = createClient(
 const ICE = { iceServers: [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
-  { urls: 'stun:stun2.l.google.com:19302' },
+  { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
+  { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
+  { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' },
 ]}
 
 type Tela = 'carregando' | 'precall' | 'espera' | 'chamada' | 'encerrado' | 'encerrada_paciente' | 'erro'
